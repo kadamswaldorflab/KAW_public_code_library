c61318edd4d3b9ba5e36bbb294439e82a7da56c6
jeffmun <jeffmun@uw.edu>
2025-11-03 14:03:37 -0800

    Refactor R code for safe statistical tests

--- R/Rcode_safe_stats.R
+++ R/Rcode_safe_stats.R
@@ -3,7 +3,7 @@ library(dplyr)
 library(rstatix)
 library(purrr)
 
-cat("###### Rcode_safe_stats.R ######\n# Use these functions to conduct pairwise tests across subsets of data. ")
+cat("###### Rcode_safe_stats.R ######\n# Use these functions to conduct pairwise tests across subsets of data... ")
 cat("\n# safe_pairwise_t_tests ")
 cat("\n# safe_pairwise_wilcox_tests ")
 
@@ -239,5 +239,6 @@ safe_pairwise_wilcox_tests <- function(df, x_var, g_var, subset_vars, ndigits=3,
   result <- safe_pairwise_tests(df, x_var, g_var, subset_vars, "Wilcox", ndigits, show_p_LT_point1)
   return(result)
 } 
+
 
 
