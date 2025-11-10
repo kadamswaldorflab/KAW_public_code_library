65c7594c34b4070f308bb315aa42a65680fa584e
jeffmun <jeffmun@uw.edu>
2025-11-10 13:30:29 -0800

    Refactor R code for safe statistics functions

--- R/Rcode_safe_stats.R
+++ R/Rcode_safe_stats.R
@@ -311,25 +311,17 @@ df_pairwisetests  <-
     , b.statistic as wilc_statistic
     , b.p as wilc_p
     , b.pstars as wilc_pstars
-    , b.sig_result as wilc_sig_result, b.test_status as wilc_test_status, b.err_msg as wilc_err_msg
+    , b.sig_result as wilc_sig_result
   from df_ttest a
     join df_wilctest b 
     ON  a.variable = b.variable
     AND a.subsetpk = b.subsetpk
     AND a.group1 = b.group1
     AND a.group2 = b.group2") %>% 
-  rename(t_statitic = statistic, t_p = p, t_pstars = pstars, t_test_status = test_status, t_err_msg = err_msg)
+  rename(t_statitic = statistic, t_p = p, t_pstars = pstars, t_sig_result = sig_result)
 
 return(df_pairwisetests)
 }
-
-
-
-
-
-
-
-
 
 
 
