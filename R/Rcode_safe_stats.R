715e444dbfde840716ff1373da255b435e872303
jeffmun <jeffmun@uw.edu>
2025-11-12 20:27:58 -0800

    Refactor R code for safe statistical tests

--- R/Rcode_safe_stats.R
+++ R/Rcode_safe_stats.R
@@ -288,25 +288,36 @@ safe_pairwise_test <- function(df, x_vars, g_var, subset_vars, testtype, ndigits
 }
 
 
-safe_pairwise_tests <- function(df, x_vars, g_var, subset_vars, testtype="both", ndigits=3, show_p_LT_point1=F) {
+safe_pairwise_tests  <- function(df, x_vars, g_var
+        , subset_vars
+        , testtype="both", ndigits=3, show_p_LT_point1=F
+        , format_stacked_or_wide = "stacked") {
 
   if(testtype!="both" & testtype!="T-test" & testtype!="Wilcoxon")
     {
        cat(paste0("\n'testtype' parameter must be one of the following: \"T-Test\", \"Wilcoxon\", \"both\". Please try again.\n"))
        return()
     }
-  
   if(testtype=="both") {
     result_t <- safe_pairwise_test(df, x_vars, g_var, subset_vars, "T-test", ndigits, show_p_LT_point1)
     result_w <- safe_pairwise_test(df, x_vars, g_var, subset_vars, "Wilcoxon", ndigits, show_p_LT_point1)
-    result  <- merge_safe_t_wilc(result_t, result_w)
+    if(format_stacked_or_wide = "stacked") { 
+        result  <- bind_rows(result_t, result_w) 
+    } else {
+        result  <- merge_safe_t_wilc(result_t, result_w)    
+    }
     } else if(testtype=="T-test") {
     result <- safe_pairwise_test(df, x_vars, g_var, subset_vars, "T-test", ndigits, show_p_LT_point1)
   } else if(testtype=="Wilcoxon") {
     result <- safe_pairwise_test(df, x_vars, g_var, subset_vars, "Wilcoxon", ndigits, show_p_LT_point1)
   }
   return(result)
-} 
+}
+
+
+
+
+
 
 merge_safe_t_wilc  <- function(df_ttest, df_wilctest){
 df_pairwisetests  <- 
@@ -325,4 +336,5 @@ df_pairwisetests  <-
 
 return(df_pairwisetests)
 }
+
 
